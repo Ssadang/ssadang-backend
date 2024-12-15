34b1ff26fee0c3fbbfc70373cb2afbc9c344e29e
이동규 <winlouder3@gmail.com>
2024-12-15 19:25:42 +0900

    Init : 초기 세팅

---
+++ src/main/java/com/ssafy/ssadang/SsadangApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssadang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsadangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsadangApplication.class, args);
+	}
+
+}
