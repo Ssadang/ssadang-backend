df420c79525ee184e9dcd2ae2615e7ad1ae3f943
5almiakki <jpui9635@gmail.com>
2024-12-17 16:56:03 +0900

    Fix : 기프티콘 엔티티 수정
    PK에 auto increment를 지정하기 위해 @GeneratedValue 추가

--- src/main/java/com/ssafy/ssadang/domain/gifticon/entity/Gifticon.java
+++ src/main/java/com/ssafy/ssadang/domain/gifticon/entity/Gifticon.java
@@ -6,6 +6,8 @@ import com.ssafy.ssadang.domain.user.entity.User;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -22,6 +24,7 @@ import lombok.NoArgsConstructor;
 public class Gifticon {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer gifticonId;
 	@JoinColumn(name = "owner_id")
 	@ManyToOne(fetch = FetchType.LAZY)
