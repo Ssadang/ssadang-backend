b3e1873f226e46644ca89d32342556b45d7b4d56
5almiakki <jpui9635@gmail.com>
2024-12-17 16:25:02 +0900

    Feat : 기프티콘 컨트롤러 추가

---
+++ src/main/java/com/ssafy/ssadang/domain/gifticon/controller/GifticonController.java
@@ -0,0 +1,25 @@
+package com.ssafy.ssadang.domain.gifticon.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.ssadang.domain.gifticon.dto.GifticonRequestDto;
+import com.ssafy.ssadang.domain.gifticon.dto.GifticonResponseDto;
+import com.ssafy.ssadang.domain.gifticon.service.GifticonService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/gifticon")
+@RequiredArgsConstructor
+public class GifticonController {
+	
+	private final GifticonService gifticonService;
+	
+	@PostMapping
+	public GifticonResponseDto save(GifticonRequestDto gifticonRequestDto) {
+		return gifticonService.save(gifticonRequestDto);
+	}
+
+}
