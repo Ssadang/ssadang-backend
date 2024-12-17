3d44583c76ff83a3819670aaae0e31ec5ebcdc8d
5almiakki <jpui9635@gmail.com>
2024-12-17 15:57:22 +0900

    Feat : 기프티콘 엔티티 추가

---
+++ src/main/java/com/ssafy/ssadang/domain/gifticon/entity/Gifticon.java
@@ -0,0 +1,34 @@
+package com.ssafy.ssadang.domain.gifticon.entity;
+
+import java.time.LocalDate;
+
+import com.ssafy.ssadang.domain.user.entity.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "gifticon")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class Gifticon {
+	
+	@Id
+	private Integer gifticonId;
+	@JoinColumn(name = "owner_id")
+	@ManyToOne(fetch = FetchType.LAZY)
+	private User owner;
+	private String imagePath;
+	private LocalDate expiryDate;
+	private String name;
+	private Boolean used;
+
+}
