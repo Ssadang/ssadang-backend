082dae43da625cbb65e398f86028708efdcac1bc
5almiakki <jpui9635@gmail.com>
2024-12-17 16:04:08 +0900

    Feat : 기프티콘 리포지토리 추가

---
+++ src/main/java/com/ssafy/ssadang/domain/gifticon/repository/GifticonRepository.java
@@ -0,0 +1,9 @@
+package com.ssafy.ssadang.domain.gifticon.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ssafy.ssadang.domain.gifticon.entity.Gifticon;
+
+public interface GifticonRepository extends JpaRepository<Gifticon, Integer> {
+
+}
