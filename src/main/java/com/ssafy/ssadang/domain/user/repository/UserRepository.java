1cf2f06ee70ad4fe15f8aa21c1fd90f1d9517877
5almiakki <jpui9635@gmail.com>
2024-12-22 09:33:03 +0900

    Remove : 브랜치 병합 전 사용자 도메인 삭제

--- src/main/java/com/ssafy/ssadang/domain/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.ssadang.domain.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ssafy.ssadang.domain.user.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
--- src/main/java/com/ssafy/ssadang/domain/user/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.ssadang.domain.user.service;
-
-import com.ssafy.ssadang.domain.user.entity.User;
-
-public interface UserService {
-	
-	User findById(Integer userId);
-
-}
