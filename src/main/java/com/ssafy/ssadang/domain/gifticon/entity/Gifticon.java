bffdcf6518b4648c13cd2fea2d081762c723e29d
5almiakki <jpui9635@gmail.com>
2024-12-28 19:30:56 +0900

    Fix : 기프티콘 엔티티 애너테이션 수정

--- src/main/java/com/ssafy/ssadang/domain/gifticon/entity/Gifticon.java
+++ src/main/java/com/ssafy/ssadang/domain/gifticon/entity/Gifticon.java
@@ -14,13 +14,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity(name = "gifticon")
+@Entity
+@Table(name = "gifticon")
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
