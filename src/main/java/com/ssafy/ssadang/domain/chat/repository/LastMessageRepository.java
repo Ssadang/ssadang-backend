c57080fdbee0ec462ae96478ddbe6e1e2f6bab7b
hyobin0726 <hyobin0726@naver.com>
2024-12-31 11:27:55 +0900

    Fix: 채팅방리스트 최신순 조회

--- src/main/java/com/ssafy/ssadang/domain/chat/repository/LastMessageRepository.java
+++ src/main/java/com/ssafy/ssadang/domain/chat/repository/LastMessageRepository.java
@@ -1,6 +1,7 @@
 package com.ssafy.ssadang.domain.chat.repository;
 
 import com.ssafy.ssadang.domain.chat.collection.LastMessage;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +12,5 @@ import java.util.Optional;
 public interface LastMessageRepository extends MongoRepository<LastMessage, String> {
     //기존 채팅방있는가 확인
     Optional<LastMessage> findByChatTypeAndSenderIdsContainingAndSaleBoardIdAndShareBoardId(Integer chatType, Integer userId, Integer saleBoardId, Integer shareBoardId);
-    List<LastMessage> findBySenderIdsContaining(Integer userId);
+    List<LastMessage> findBySenderIdsContaining(Integer userId, Sort sort);
 }
