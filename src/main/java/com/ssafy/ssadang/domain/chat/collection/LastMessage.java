3e296e3c918495ae072d9bab7bba704c40191a79
hyobin0726 <hyobin0726@naver.com>
2024-12-31 10:47:15 +0900

    fix: 채팅생성 수정

--- src/main/java/com/ssafy/ssadang/domain/chat/collection/LastMessage.java
+++ src/main/java/com/ssafy/ssadang/domain/chat/collection/LastMessage.java
@@ -14,7 +14,6 @@ import java.util.Set;
 public class LastMessage {
     @Id
     private String id;
-    private Integer chatRoomId;
     private String content;
     private LocalDateTime createDate;
     private Set<Integer> senderIds;
--- src/main/java/com/ssafy/ssadang/domain/chat/repository/LastMessageRepository.java
+++ src/main/java/com/ssafy/ssadang/domain/chat/repository/LastMessageRepository.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface LastMessageRepository extends MongoRepository<LastMessage, String> {
-    Optional<LastMessage> findByChatRoomId(Integer chatRoomId);
+    //기존 채팅방있는가 확인
+    Optional<LastMessage> findByChatTypeAndSenderIdsContainingAndSaleBoardIdAndShareBoardId(Integer chatType, Integer userId, Integer saleBoardId, Integer shareBoardId);
     List<LastMessage> findBySenderIdsContaining(Integer userId);
 }
