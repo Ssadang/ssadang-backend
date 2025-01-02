bf87550754d0843a82730b48a16e0691a1e1dea5
hyobin0726 <hyobin0726@naver.com>
2025-01-02 17:41:35 +0900

    Fix : 채팅내역 페이지네이션

--- src/main/java/com/ssafy/ssadang/domain/chat/service/ChatMessageService.java
+++ src/main/java/com/ssafy/ssadang/domain/chat/service/ChatMessageService.java
@@ -3,11 +3,13 @@ package com.ssafy.ssadang.domain.chat.service;
 import com.ssafy.ssadang.domain.chat.collection.ChatMessage;
 import com.ssafy.ssadang.domain.chat.dto.ChatMessageRequestDto;
 import com.ssafy.ssadang.domain.chat.dto.ChatMessageResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface ChatMessageService {
     ChatMessageResponseDto sendMessage(ChatMessageRequestDto request, String chatRoomId);
-    List<ChatMessage> getChatMessagesByChatRoomId(String chatRoomId, Integer userId);
+    Page<ChatMessage> getChatMessagesByChatRoomId(String chatRoomId, Integer userId, Pageable pageable);
 
 }
