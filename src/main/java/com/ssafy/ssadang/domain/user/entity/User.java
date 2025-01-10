28d30390bce7b04c78b4cb623d6019a4ddd3dc1f
unknown <winlouder3@gmail.com>
2025-01-10 12:44:28 +0900

    Feat : 유정 정보 받아오기

--- src/main/java/com/ssafy/ssadang/domain/user/entity/User.java
+++ src/main/java/com/ssafy/ssadang/domain/user/entity/User.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -52,5 +54,6 @@ public class User {
 	private int shareCount;
 
 	@OneToMany(mappedBy = "user")
+	@JsonManagedReference
 	private List<RoleRegister> roleRegister;
 }
